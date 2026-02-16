9f39e631649c3bb274772ddb360da86c2f14e5c3
AyoubSasi20 <Ayoub.eswie@outlook.com>
2026-02-16 16:55:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const product = [{name: 'Product1', price: 10},
+               {name: 'Product2', price: 15},
+               {name: 'Product3', price: 20}];
+const Tax = 0.15;
+
+
+
+
+console.log(product)
