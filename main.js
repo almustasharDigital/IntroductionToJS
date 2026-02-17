a5386ca49711b5dce8f0cba43113063882c7e611
AyoubSasi20 <Ayoub.eswie@outlook.com>
2026-02-17 10:58:29 +0200

    aaa

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const product = [{name: 'Product1', price: 10},
                {name: 'Product2', price: 15},
                {name: 'Product3', price: 20}];
-const Tax = 0.15;
+const productwithtax = product.map(product => {return {name: product.name, price: product.price * 1.15}});
 
 
-
-
-console.log(product)
+console.log('product =',product)
+console.log('producteith tax =',productwithtax)
